55e3b033eb3943a51e394cf965624264564bb6af
Hectorrojas150 <hectorrojas150@yahoo.com.ar>
2024-10-04 08:47:00 -0300

    Realizamos un programa que según la edad nos indica si podemos ingresar a una discoteca para practicar diagrama de flujos, pseudocodigo y algoritmos

---
+++ ingreso-permitido.py
@@ -0,0 +1,13 @@
+# 1.Solicitar al usuario que ingrese la edad del cliente
+edad = int(input ("Por favor, ingresa la edad del cliente: "))
+
+# 2. Verificar si la edad ingresada cumple con el requisito para ingresar a la discoteca
+permitido = True if  edad >= 18 else False
+
+#3. Mostrar al usuario si su cliente puede o no ingresar a la discoteca
+
+if permitido:
+    print ("¡Puedes ingresar a la discoteca!")
+
+else:
+    print ("Lo sentimos muchos, no se puede ingresar a la discoteca siendo menor de edad")
